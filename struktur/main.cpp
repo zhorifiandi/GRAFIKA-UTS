1fd229b59b7a1d6e6967872e5eb853b8bb6c1669
Ari <zhorifiandi@gmail.com>
2017-04-03 07:10:20 +0700

    garis miring 80% bisa

--- struktur/main.cpp
+++ struktur/main.cpp
@@ -31,9 +31,9 @@ void insertPriority(int j,int i, int p){
 		sort(polygonMatrix[i][j].begin(),polygonMatrix[i][j].end());
 }
 
-void insertLine_Matrix(float x1, float y1,float x2, float y2, float priority)
+void insertLine_Matrix(int x1, int y1,int x2, int y2, int priority)
 {
-	// Bresenham's line algorithm
+	//Bresenham's line algorithm
 	const bool steep = (fabs(y2 - y1) > fabs(x2 - x1));
 	if(steep)
 	{
@@ -75,7 +75,24 @@ void insertLine_Matrix(float x1, float y1,float x2, float y2, float priority)
 	      error += dx;
 	  }
 	}	
-	
+
+	// int m_new = 2 *(y2-y1);
+	// int slope_error_new = m_new - (x2 - x1);
+	// for (int x = x1, y = y1; x <= x2; x++)
+	// {
+	// 	insertPriority(y,x,priority);
+ 
+	// 	// Add slope to increment angle formed
+	// 	slope_error_new += m_new;
+
+	// 	// Slope error reached limit, time to
+	// 	// increment y and update slope error.
+	// 	if (slope_error_new >= 0)
+	// 	{
+	// 	 y++;
+	// 	 slope_error_new  -= 2 * (x2 - x1);
+	// 	}
+ //   }
 
 }
 
@@ -96,8 +113,13 @@ void deleteFromPrio(int p){
 void showOverlap(){
 	int current_priority = -1;
 	Framebuffer fb;
+	bool isNeedCheck = false;
+	int checker = -1;
+
 	for (int i=0;i<MATSIZE;i++){//y
 		for (int j=0;j<MATSIZE;j++){//x
+
+
 			if (polygonMatrix[i][j].empty()){
 				if (priority_container.empty()){
 					//kosong
@@ -105,22 +127,31 @@ void showOverlap(){
 				else {
 					auto elmt = color_container.at(priority_container.front()-1);
 					fb.putPixel(j,i,elmt.r,elmt.g,elmt.b);
+					if (j == MATSIZE -1){
+						cout << "Kena ujung" << endl;
+					}
 				}
 			}
 			else {
 				//Border Pertama ketemu
-				// cout << "notempty ";
+				cout << "notempty ";
 				if (priority_container.empty()){
-					// cout << "empty" << endl;
-					priority_container.push_back(polygonMatrix[i][j].front());
-					cout << polygonMatrix[i][j].front() << endl;
-					auto elmt = color_container.at(priority_container.front()-1);
-					fb.putPixel(j,i,elmt.r,elmt.g,elmt.b);
+					cout << "empty" << endl;
+					int temp; cin >> temp;
+					if (checker == polygonMatrix[i][j].front()){
+							//Skip
+					}
+					else {
+						priority_container.push_back(polygonMatrix[i][j].front());
+						auto elmt = color_container.at(priority_container.front()-1);
+						fb.putPixel(j,i,elmt.r,elmt.g,elmt.b);
+						checker = -1;
+					}
 					// cout << "ini?" << endl;
 				}
 				//Ujung dari bordernya
 				else if (priority_container.front() == polygonMatrix[i][j].front()){
-					// cout << "n1" << endl;
+					cout << "n1" << endl;
 					int temp; cin >> temp;
 					temp = priority_container.front();
 					for (auto it = polygonMatrix[i][j].begin(); it != polygonMatrix[i][j].end(); it++){
@@ -133,12 +164,16 @@ void showOverlap(){
 					auto elmt = color_container.at(priority_container.front()-1);
 					fb.putPixel(j,i,elmt.r,elmt.g,elmt.b);
 					// cout <<"len "<< priority_container.size() << endl;
-					deleteFromPrio(temp);
+					
 					// cout <<"len "<< priority_container.size() << endl;
+					isNeedCheck = true;
+					checker =priority_container.front();
+					deleteFromPrio(temp);
 				}
 				//Belom sampe ujungnya, tapi nemu ujung prioritas lain
 				else {
 					//Awal border dari suatu objek
+					cout << "n2" << endl;
 					bool isLastBorderExist = false;
 					std::vector<int> lastborders;
 					if (!polygonMatrix[i][j].empty()){
@@ -164,77 +199,14 @@ void showOverlap(){
 					}
 				}
 			}
+			if (j == MATSIZE -1){
+				checker = -1;
+			}
 		}
 	}
 
 }
 
-// bool isExist(int p){
-// 	for (std::list<int>::iterator it = colorMode.begin(); it != colorMode.end(); ++it){
-// 		if (p==*it) return true;		
-// 	}
-// 	return false;
-// }
-
-// bool isExistPList(int row, int col, int p){
-// 	for (int i =0; i<polygonMatrix[col][row].size();i++){
-// 		if (polygonMatrix[col][row][i]==p) return true;
-// 		}
-// 	return false;
-// }
-
-// bool isExistRow(int row,int col,int p){
-// 		for (int j = row;j<MATSIZE;j++)
-// 			if (isExistPList(j,col,p)) return true;
-// 		return false;
-// 	}
-
-// void showOverlap(){
-// 	Framebuffer fb1;
-// 	colorMode.push_front(0);
-// 	for (int i=0;i<MATSIZE;i++){//y
-// 		for (int j=0;j<MATSIZE;j++){//x
-// 			if (colorMode.front()==0 && polygonMatrix[i][j][0]==9) int p=0; //Tidak ada mode pewarnaan & idx matrix kosong(tidak di area gambar)
-			
-// 			else if (colorMode.front()==0 && polygonMatrix[i][j][0]!=9){//border terluar
-// 				int k =0;
-// 				while (k<polygonMatrix[i][j].size()){
-// 					if (!isExist(polygonMatrix[i][j][k]) && polygonMatrix[i][j][k]!=9 &&  isExistRow(j,i,polygonMatrix[i][j][k])) 
-// 						colorMode.push_front(polygonMatrix[i][j][k]);
-// 					k++;
-// 				}
-// 				colorMode.remove(0);
-// 				colorMode.sort();
-// 				fb1.putPixel(j, i, color_container[colorMode.front()].r,color_container[colorMode.front()].g,color_container[colorMode.front()].b);
-// 				colorMode.push_back(0);
-// 			}
-			
-// 			else if (colorMode.front()!=0){//ada mode warna
-// 				if (polygonMatrix[i][j][0]==9) fb1.putPixel(j, i, color_container[colorMode.front()].r,color_container[colorMode.front()].g,color_container[colorMode.front()].b);//warna sel kosong
-// 				else {//sel punya warna
-// 					int priority = polygonMatrix[j][i][0];
-// 					if (priority>colorMode.front()) priority = colorMode.front();
-// 					fb1.putPixel(j, i, color_container[colorMode.front()].r,color_container[colorMode.front()].g,color_container[colorMode.front()].b);
-					
-// 					//usleep(50000);
-// 					int k =0;
-// 					while (k<polygonMatrix[i][j].size()){
-// 						if (!isExist(polygonMatrix[i][j][k])) 
-// 							colorMode.push_back(polygonMatrix[i][j][k]);
-// 						else if (isExist(polygonMatrix[i][j][k])) 
-// 							colorMode.remove(polygonMatrix[i][j][k]);
-// 						k++;
-// 					}
-					
-// 					colorMode.remove(9);
-// 					colorMode.remove(0);
-// 					colorMode.sort();
-// 					colorMode.push_back(0);				
-// 				}						
-// 			}
-// 		}
-// 	}
-// }
 
 void drawKotak(int deltax, int deltay, color c, int priority){
 	insertLine_Matrix(deltax+200+50 , deltay+100 , deltax+200+50 , deltay+200 ,priority);
@@ -244,28 +216,32 @@ void drawKotak(int deltax, int deltay, color c, int priority){
 	color_container.push_back(c);
 }
 
-
+void drawSegitiga(int deltax, int deltay, color c, int priority){
+	insertLine_Matrix(deltax+100+50 , deltay+100 , deltax+200+50 , deltay+200 ,priority);
+	//insertLine_Matrix(deltax+200+50 , deltay+200 , deltax+410-50 , deltay+200 ,2);
+//	insertLine_Matrix(deltax+200+50 , deltay+100 , deltax+410-51 , deltay+100 ,2);
+	insertLine_Matrix(deltax+201+50 , deltay+100 , deltax+410-50 , deltay+200 ,priority);
+	color_container.push_back(c);
+}
 
 
 int main(){
 	Line l;
 	int r,g,b = 255;
-	cout << "smtg" << endl;
 	// l.insertLine(100, 50, 50, 100, r,g,b);
 	// l.insertLine(50, 100, 100, 100, r,g,b);
 	// l.insertLine(100, 100, 150, 50, r,g,b);
-	cout << "smtg" << endl;
 	color c;
 	// KASUS INI ENGGAK
-	c.r = 5;
-	c.g = 32;
-	c.b = 164;
-	drawKotak(0,40,c,1);
+	// c.r = 5;
+	// c.g = 32;
+	// c.b = 164;
+	// drawKotak(0,40,c,1);
 
-	c.r = 45;
-	c.g = 255;
-	c.b = 64;
-	drawKotak(-70,30,c,2);
+	// c.r = 45;
+	// c.g = 255;
+	// c.b = 64;
+	// drawKotak(-70,30,c,2);
 
 	// c.r = 255;
 	// c.g = 255;
@@ -286,10 +262,15 @@ int main(){
 
 
 //Abaikan ini
-	c.r = 58;
-	c.g = 132;
-	c.b = 94;
-	drawKotak(10,80,c,3);
+	// c.r = 58;
+	// c.g = 132;
+	// c.b = 94;
+	// drawKotak(10,80,c,3);
+
+	c.r = 158;
+	c.g = 13;
+	c.b = 4;
+	drawSegitiga(110,80,c,1);
 
 
 	
